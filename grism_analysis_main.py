b7228343c01323bd6f112bc38cd070f667699371
WGolay <wgolay30@gmail.com>
2022-04-02 21:37:53 -0500

    Fix calibration file finder

--- grism_analysis_main.py
+++ grism_analysis_main.py
@@ -14,13 +14,16 @@ import astropy.io.fits as pyfits
 def main():
     web_analyzer = grism_web()
     fits_image = web_analyzer.get_fits(web_analyzer) # Get initial fits image
+
+    with open('temp/temp.fts', 'wb') as binary_file: # Write fits image to file so it can be analyzed
+        binary_file.write(fits_image['content'])
     
     if not False: # TODO: Add advanced option on first page for entry of custom calibration file, otherwise search for one
         defaultDir = 'calibrations/'
         
         # Get date of image to iterate back to latest calibration file, parse header into date object
-        im, hdr = pyfits.getdata(fits_image, 0, header=True)
-        fitsDate = hdr['DATE-OBS']
+        hdulist = pyfits.open('temp/temp.fts')
+        fitsDate = hdulist[0].header['DATE-OBS']
         startDate = date(int(fitsDate[0:4]), int(fitsDate[5:7]), int(fitsDate[8:10]))
         
         # Iterate to find latest calib file in last 180 days
@@ -33,11 +36,7 @@ def main():
     if not os.path.exists(cal_file):
         raise Exception('Calibration file not located!') # ? Is there a way to do popups instead of breaking?
     
-    # ? Does this need to be done? The image isn't actually modified when it is being analyzed. 
-    with open("temp.fts", "wb") as binary_file: # Write fits image to file so it can be analyzed
-        binary_file.write(fits_image['content'])
-    
-    grism_analyzer = grism_tools('temp.fts', cal_file) # instantiate analyzer with fits image and calibration file
+    grism_analyzer = grism_tools('temp/temp.fts', cal_file) # instantiate analyzer with fits image and calibration file
     web_analyzer.run_analysis(web_analyzer, grism_analyzer)
 
 if __name__ == '__main__':
